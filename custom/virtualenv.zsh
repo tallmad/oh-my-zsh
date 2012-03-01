ab7741c28676580eec3916e3269a5114762b680b
lxyu <lx.yu@ele.me>
2012-03-01 20:39:53 +0800

    add virtualenv location for linux.

--- custom/virtualenv.zsh
+++ custom/virtualenv.zsh
@@ -3,3 +3,4 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/workspace
 
 [[ -s "/usr/local/share/python/virtualenvwrapper.sh" ]] && source "/usr/local/share/python/virtualenvwrapper.sh"
+[[ -s "/usr/bin/virtualenvwrapper.sh" ]] && source "/usr/bin/virtualenvwrapper.sh"
