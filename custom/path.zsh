02e5dc850f25a87ebdf7b84892d31bc081422f6f
lxyu <lx.yu@ele.me>
2012-03-01 15:37:06 +0800

    fix path not exist in linux env issue.

--- custom/path.zsh
+++ custom/path.zsh
@@ -1,5 +1,9 @@
-export PATH=/usr/local/share/python:$PATH
-export PATH=/usr/local/sbin:$PATH
-export PATH=/usr/local/bin:$PATH
-export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
-export PATH=$HOME/bin/:$PATH
+export PATH=$HOME/bin/:/usr/local/bin:/usr/local/sbin:$PATH
+
+# in case brew installed a newer version of python.
+[[ -d "/usr/local/share/python" ]] && \
+    export PATH=/usr/local/share/python:$PATH
+
+# test brew exists
+[[ -f "/usr/local/bin/brew" ]] && \
+    export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
