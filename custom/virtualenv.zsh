0b729bcf0dc686822cda98921e07d0965e34c686
lxyu <lx.yu@ele.me>
2012-03-02 12:07:27 +0800

    add two env for virtualenv.

--- custom/virtualenv.zsh
+++ custom/virtualenv.zsh
@@ -2,5 +2,10 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/workspace
 
+# source virtualenvwrapper.sh
 [[ -s "/usr/local/share/python/virtualenvwrapper.sh" ]] && source "/usr/local/share/python/virtualenvwrapper.sh"
 [[ -s "/usr/bin/virtualenvwrapper.sh" ]] && source "/usr/bin/virtualenvwrapper.sh"
+
+# make pip use the same directory for virtualenvs as virtualenvwrapper
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+export PIP_RESPECT_VIRTUALENV=true
