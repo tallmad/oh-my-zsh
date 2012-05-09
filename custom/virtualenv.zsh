f3dd05f6e16f5e072e72e07cc7c3ff97e7fe58a3
lxyu <lx.yu@ele.me>
2012-05-09 16:05:15 +0800

    refine how virtualenvwrapper.sh is sourced.

--- custom/virtualenv.zsh
+++ custom/virtualenv.zsh
@@ -1,10 +1,11 @@
+# source virtualenvwrapper.sh
+if which virtualenvwrapper.sh &> /dev/null; then
+    source virtualenvwrapper.sh
+fi
+
 # Load virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/workspace
-
-# source virtualenvwrapper.sh
-[[ -s "/usr/local/share/python/virtualenvwrapper.sh" ]] && source "/usr/local/share/python/virtualenvwrapper.sh"
-[[ -s "/usr/bin/virtualenvwrapper.sh" ]] && source "/usr/bin/virtualenvwrapper.sh"
 
 # make pip use the same directory for virtualenvs as virtualenvwrapper
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
