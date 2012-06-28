db6645177da1d49a59180fe72bbdd22fe87d579c
lxyu <lx.yu@ele.me>
2012-06-28 15:15:01 +0800

    change how PATH env defined.
    1. explicitly set PATH because Mac's path_helper seems to mess up the
    PATH, add too much duplicate rubbish to it.
    2. add android and homebin to PATH

--- custom/path.zsh
+++ custom/path.zsh
@@ -1,4 +1,4 @@
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # in case brew installed a newer version of python.
 [[ -d "/usr/local/share/python" ]] && \
@@ -7,3 +7,10 @@ export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
 # test brew exists
 [[ -f "/usr/local/bin/brew" ]] && \
     export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+# test android sdk
+[[ -d "$HOME/tools/android/platform-tools" ]] && \
+    export PATH=$HOME/tools/android/platform-tools:$HOME/tools/android/platform:$HOME/tools/android/tools:$PATH
+
+# test user bin
+[[ -d "$HOME/bin" ]] && export PATH=$HOME/bin:$PATH
