154bef1d9fca5b05fea99f28148382a0ac35ef57
lxyu <lixinfish@gmail.com>
2012-10-15 16:12:02 +0800

    update aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -19,9 +19,8 @@ alias rmds='find . -name ".DS_Store" -delete'
 alias rmpyc='find . -name "*.pyc" -delete'
 
 alias s='pkill -f "ssh -Nfn -D 7070"; nohup ssh -Nfn -D 7070 elemevps 1>/dev/null 2>&1'
-alias wk='nohup python2.6 ~/tools/wiki/wikiserver.py 1>/dev/null 2>&1 &'
-alias p='cd ~/tools/goagent/; nohup python proxy.py 1>/dev/null 2>&1 &'
-alias vpn='cd ~/tools/sshuttle; bash sshuttle --dns -vvr elemevps 0/0'
+alias wk='pkill -f "wikiserver.py"; nohup python2.6 ~/tools/wiki/wikiserver.py 1>/dev/null 2>&1 &'
+alias p='pkill -f "python proxy.py"; cd ~/tools/goagent/local/; nohup python proxy.py 1>/dev/null 2>&1 &'
 alias y='youtube-dl -c -l'
 
 alias xld='xl download --continue --delete --tool aria2'
@@ -35,3 +34,5 @@ alias cdze='workon zeusenv; cd ~/workspace/eleme/zeus/'
 alias cdma='workon mangixenv; cd ~/workspace/eleme/mangix/'
 alias cdme='workon mechanizebotenv; cd ~/workspace/eleme/mechanizebot/'
 alias cdes='workon earthshakerenv; cd ~/workspace/eleme/earthshaker/'
+
+alias redis-server='redis-server /usr/local/etc/redis.conf'
