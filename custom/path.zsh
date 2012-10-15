27f0863e61bba12c215ea9496080f7a27b626467
lxyu <lixinfish@gmail.com>
2012-10-15 16:12:14 +0800

    refine path env config with if statement

--- custom/path.zsh
+++ custom/path.zsh
@@ -1,16 +1,25 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# in case brew installed a newer version of python.
-[[ -d "/usr/local/share/python" ]] && \
-    export PATH=/usr/local/share/python:$PATH
+if [[ -f "/usr/local/bin/brew" ]]; then
+    if [[ -d "$(brew --prefix coreutils)" ]]; then
+        export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+    fi
 
-# test brew exists
-[[ -f "/usr/local/bin/brew" ]] && \
-    export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+    if [[ -d "$(brew --prefix python)" ]]; then
+        export PATH=$(brew --prefix python)/bin:$PATH
+    fi
+
+    if [[ -d "$(brew --prefix ruby)" ]]; then
+        export PATH=$(brew --prefix ruby)/bin:$PATH
+    fi
+fi
 
 # test android sdk
-[[ -d "$HOME/tools/android/platform-tools" ]] && \
+if [[ -d "$HOME/tools/android/platform-tools" ]]; then
     export PATH=$HOME/tools/android/platform-tools:$HOME/tools/android/platform:$HOME/tools/android/tools:$PATH
+fi
 
 # test user bin
-[[ -d "$HOME/bin" ]] && export PATH=$HOME/bin:$PATH
+if [[ -d "$HOME/bin" ]]; then
+    export PATH=$HOME/bin:$PATH
+fi
