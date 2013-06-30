5a98ce8e1c134a3275e5ff2d860759005922df64
Lx Yu <lixinfish@gmail.com>
2013-06-30 15:19:36 +0800

    plugin update

--- custom/env.zsh
+++ custom/env.zsh
@@ -10,19 +10,6 @@ export CLICOLOR=1
 # fix gevent block bug
 export GEVENT_RESOLVER=block
 
-# make man colorful
-function man() {
-    env \
-        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-        LESS_TERMCAP_md=$(printf "\e[1;31m") \
-        LESS_TERMCAP_me=$(printf "\e[0m") \
-        LESS_TERMCAP_se=$(printf "\e[0m") \
-        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-        LESS_TERMCAP_ue=$(printf "\e[0m") \
-        LESS_TERMCAP_us=$(printf "\e[1;32m") \
-            man "$@"
-}
-
 # auto verify add config git user name and email
 function git_user_verify() {
     if [[ -d ".git" ]]; then
